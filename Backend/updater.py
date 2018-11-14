cec9a0c442cc2004999ac2e64d63661b07dd390f
Samuel Novák <samuel.novak@protonmail.com>
2018-11-14 23:45:43 +0000

    Popen

--- Backend/updater.py
+++ Backend/updater.py
@@ -1,5 +1,5 @@
 from flask import abort
-from os import system
+from subprocess import Popen, PIPE
 from webhook import webhook
 
 def update_hook(headers:dict, payload:dict, config:dict):
@@ -19,7 +19,7 @@ def update_hook(headers:dict, payload:dict, config:dict):
 
 def run_update(payload:dict, upstream:str, branch:str):
     webhook("Updater: push event registered.")
-    m = system("git fetch {} {}".format(upstream, branch)) + "\n"
-    m += system("git pull {} {}".format(upstream, branch))
+    m = Popen("git fetch {} {}".format(upstream, branch), stdout=PIPE).communicate()[0] + "\n"
+    m += Popen("git pull {} {}".format(upstream, branch), stdout=PIPE).communicate()[0]
     webhook(m)
     print(m)
